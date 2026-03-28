5c5ea9d16c489be39d3877ce5c585a3b551a6a42
ronak228 <ronakgadhiya800@gmail.com>
2026-03-28 09:21:28 +0530

    Deploy in Render

--- app.py
+++ app.py
@@ -1100,5 +1100,6 @@ def not_found(_):
     return "Not Found",404
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT",10000)), debug=False)
+# ✅ Correct — Vercel picks up the `app` variable
+if __name__ == '__main__':
+    app.run(debug=True)
